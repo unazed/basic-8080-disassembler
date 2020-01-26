9024df453fd1ca4cc33c724603d1d22276885621
Unazed Spectaculum <leethax0r.coder@gmail.com>
2020-01-26 17:30:19 +0000

    rjust on instr. offset, consistent ascii dump

--- example.py
+++ example.py
@@ -7,7 +7,7 @@ if __name__ != "__main__":
 
 with open("example.com", 'rb') as data:
     data = data.read()
-disasm = unazed_disasm.Disassembler(data, unazed_disasm.OPCODE_MAP, org=0x100)
+disasm = unazed_disasm.Disassembler(data, unazed_disasm.OPCODE_MAP)
 offs = 0
 for instr, *operands in disasm.iterate_instructions():
     bytes_ = f"{format(ord(instr.byte_ident), 'x').rjust(2,'0'):4s}"
@@ -16,5 +16,5 @@ for instr, *operands in disasm.iterate_instructions():
         _ = [_[i:i+2] for i in range(0, len(_), 2) ]
         bytes_ += ' '.join(_)
     ascii_ = [k if k in string.ascii_letters else '.' for k in map(chr, map(lambda x: int(x, 16), bytes_.split()))]
-    print(f"+{format(offs, 'x'):10s} {bytes_:20s} {instr.op_ident % operands[0] if operands else instr.op_ident:20s} {''.join(ascii_):4s} {instr._note}")
+    print(f"+{format(offs, 'x').rjust(4, '0'):10s} {bytes_:20s} {instr.op_ident % operands[0] if operands else instr.op_ident:20s} {''.join(ascii_).ljust(3, '.'):4s} {instr._note}")
     offs += instr.byte_size + 1
